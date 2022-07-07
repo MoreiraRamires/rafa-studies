a9968d58b0976e9b9bc3f264d845350e1017e495
Ramires <moreira.ramires@gmail.com>
2022-07-07 16:26:05 -0300

    add estados

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './Lista.module.scss'
 import ItemLista from "./Item";
 
 function Lista(){
-  const tarefas = [
-    {
-      tarefa:'Javascript',
-      tempo:'01:30:00'
-    },
-    {
-      tarefa:'React',
-      tempo:'00:30:00'
-    },
-    {
-      tarefa:'VUE',
-      tempo:'00:45:00'
-    },
+  const [tarefas,setTarefas] = useState([{
+    tarefa:'Javascript',
+    tempo:'01:30:00'
+  },
+  {
+    tarefa:'React',
+    tempo:'00:30:00'
+  },
+  {
+    tarefa:'VUE',
+    tempo:'00:45:00'
+  }]);
 
-  ]
+  
   return(
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={()=> {
+        setTarefas([...tarefas , {tarefa:"estudar estado",tempo:'00:10:00'}])
+      }}>Estudos do dia</h2>
       <ul>
         {tarefas.map( (atributos,index) => (
          <ItemLista key={index} tarefa={atributos.tarefa} tempo={atributos.tempo}/>
