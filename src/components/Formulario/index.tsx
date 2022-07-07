a657d24c57894519bab383defdcc7a8d7d42ff56
Ramires <moreira.ramires@gmail.com>
2022-07-07 16:46:05 -0300

    estado no input

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,3 +1,4 @@
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React from "react";
 import Botao from "../Botao";
 import style from './Formulario.module.scss'
@@ -16,11 +17,11 @@ class Formulario extends React.Component {
       <form action="" className={style.novaTarefa}>
           <div className={style.inputContainer}>
             <label htmlFor="tarefa">Adicione um novo estudo</label>
-            <input type="text" name="tarefa" id="tarefa" placeholder="O que vou estudar ..." value={this.state.tarefa}required/>
+            <input type="text" name="tarefa" id="tarefa" placeholder="O que vou estudar ..." value={this.state.tarefa} onChange={evento => this.setState({...this.state , tarefa:evento.target.value})} required/>
           </div>
           <div className={style.inputContainer}>
             <label htmlFor="tempo">Tempo</label>
-            <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00"  value={this.state.tempo}/>
+            <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00"  value={this.state.tempo} onChange={(evento)=> this.setState({...this.state, tempo:evento.target.value  })}/>
           </div>
             <Botao>
             Adicionar
